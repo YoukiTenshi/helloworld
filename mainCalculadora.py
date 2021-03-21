f3437fa10e23cccb668f30d5a381dc40efaf171b
YoukiTenshi <79215076+YoukiTenshi@users.noreply.github.com>
2021-03-20 21:12:23 -0300

    Fiz o esqueleto
    mas talvez bugue os imports

--- mainCalculadora.py
+++ mainCalculadora.py
@@ -1,1 +1,27 @@
+from . import somsub
+from . import muldiv
+
+def printCute(resultado):
+    print('Resultado:  ',resultado)
+
 print('Essa é a Calculeira')
+calculo = input('O que deseja fazer? Digite: "so" para soma, "su" para subtração, "mu" para multiplicação ou "di" para divisão:  ')
+
+a = input('Insira o primeiro valor:  ')
+b = input('Insira o segundo valor:  ')
+
+if calculo = 'so':
+    res = somsub.somar(a,b)
+    printCute(res)
+    
+if calculo = 'su':
+    res = somsub.subtrair(a,b)
+    printCute(res)
+
+if calculo = 'mu':
+    res = muldiv.multiplicar(a,b)
+    printCute(res)
+
+if calculo = 'di':
+    res = muldiv.dividir(a,b)
+    printCute(res)
